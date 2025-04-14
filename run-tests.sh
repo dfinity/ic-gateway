d4a12972851f7630f0666549a95e97d3cf238a4e
Nikolay Komarevskiy <nikolay.komarevskiy@dfinity.org>
2025-04-14 13:40:13 +0000

    fix: asset canister checksum

--- run-tests.sh
+++ run-tests.sh
@@ -4,7 +4,7 @@ set -eEuo pipefail
 readonly POCKETIC_VERSION="8.0.0"
 readonly POCKETIC_URL="https://github.com/dfinity/pocketic/releases/download/${POCKETIC_VERSION}/pocket-ic-x86_64-linux.gz"
 readonly POCKETIC_CHECKSUM="7689eee0a17abb24c0a83e2ff0ea36fd5ba7eb699fe811d9f7b07cb27e8e7170"
-readonly ASSET_WASM_URL="https://github.com/dfinity/sdk/raw/master/src/distributed/assetstorage.wasm.gz"
+readonly ASSET_WASM_URL="https://github.com/dfinity/sdk/raw/fec030f53814e7eaa2f869189e8852b5c0e60e5e/src/distributed/assetstorage.wasm.gz"
 readonly ASSET_WASM_CHECKSUM="865eb25df5a6d857147e078bb33c727797957247f7af2635846d65c5397b36a6"
 readonly WORKDIR="$(pwd)"
 readonly CANISTER_DIR="${WORKDIR}/canister_wasms"
